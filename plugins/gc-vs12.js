ef8b004f726dce67900f5b55b198a7faf526eaee
goku2812 <cayancela730@gmail.com>
2022-10-31 12:02:32 -0500

    iniciado

---
+++ plugins/gc-vs12.js
@@ -0,0 +1,56 @@
+let handler = async (m, { conn, text, command, usedPrefix }) => {
+let pp = './src/warn.jpg'
+let who
+if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
+else who = m.chat
+let user = global.db.data.users[who]
+let bot = global.db.data.settings[conn.user.jid] || {}
+let warntext = `*╔═══*💥 *12 VS 12*  *EXTERNO*💥
+*╟❧*  *HiGHER ANGELS*
+*╟*
+*╟❧* ⏰  * PM*  🇲🇽🇪🇨*
+*╟* 
+*╟❧*  *MAPA ABIERTO*
+*╟*
+*╟❧* *USO DE DISCORD OBLIGATORIO*
+*╟*
+*╚══* *ESCUADRA* 
+*╟❧*🐤
+*╟❧*🐤 
+*╟❧*🐤
+*╟❧*🐤
+*╔══*  *ESCUADRA*
+*╟❧*🐤 
+*╟❧*🐤
+*╟❧*🐤
+*╟❧*🐤
+*╔══* *ESCUADRA*
+*╟❧*🐤
+*╟❧*🐤 
+*╟❧*🐤
+*╟❧*🐤
+*╚══* *SUPLENTES*
+*╟❧*🐣
+*╟❧*🐣 
+*╚══* *DONADOR DE SALA*
+*╟❧*⭐  
+*╚═*⭐`
+if (!who) throw m.reply(warntext, m.chat, { mentions: conn.parseMention(warntext)}) 
+user.warn += 1
+  
+await conn.sendButton(m.chat,`${user.warn == 1 ? `*@${who.split`@`[0]}*` : `*@${who.split`@`[0]}*`} 𝚁𝙴𝙲𝙸𝙱𝙸𝙾 𝚄𝙽𝙰 𝙰𝙳𝚅𝙴𝚁𝚃𝙴𝙽𝙲𝙸𝙰 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾!`, `*ADVERTENCIAS ${user.warn}/3*\n\n${wm}`, pp, [['📋 𝙻𝙸𝚂𝚃𝚆𝙰𝚁𝙽 📋', '#listwarn']], m, { mentions: [who] })
+	
+if (user.warn >= 3) {
+if (!bot.restrict) return m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃 𝙽𝙾 𝚃𝙸𝙴𝙽𝙴 𝙷𝙰𝙱𝙸𝙻𝙸𝚃𝙰𝙳𝙾 𝙻𝙰𝚂 𝚁𝙴𝚂𝚃𝚁𝙸𝙲𝙲𝙸𝙾𝙽𝙴𝚂 (#𝚎𝚗𝚊𝚋𝚕𝚎 𝚛𝚎𝚜𝚝𝚛𝚒𝚌𝚝) 𝙲𝙾𝙽𝚃𝙰𝙲𝚃𝙴 𝙲𝙾𝙽 𝙴𝙻 𝙿𝙰𝚁𝙰 𝚀𝚄𝙴 𝙻𝙾 𝙷𝙰𝙱𝙸𝙻𝙸𝚃𝙴*')        
+user.warn = 0
+await m.reply(`𝚃𝙴 𝙻𝙾 𝙰𝙳𝚅𝙴𝚁𝚃𝙸 𝚅𝙰𝚁𝙸𝙰𝚂 𝚅𝙴𝙲𝙴𝚂!!\n*@${who.split`@`[0]}* 𝚂𝚄𝙿𝙴𝚁𝙰𝚂𝚃𝙴 𝙻𝙰𝚂 *3* 𝙰𝙳𝚅𝙴𝚁𝚃𝙴𝙽𝙲𝙸𝙰𝚂, 𝙰𝙷𝙾𝚁𝙰 𝚂𝙴𝚁𝙰𝚂 𝙴𝙻𝙸𝙼𝙸𝙽𝙰𝙳𝙾/𝙰 👽`, null, { mentions: [who]})
+//user.banned = true
+await conn.groupParticipantsUpdate(m.chat, [who], 'remove') 
+} 
+return !1
+}
+handler.help = ['inicial <texto>']
+handler.tags = ['group']
+handler.command = /^(vs12|VS12)$/i
+handler.botAdmin = true
+export default handler
---
+++ start.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+GREEN='\033[0;32m'
+while : 
+do
+echo "${GREEN} GOKU Bot Esta Iniciando..... 
+Auto RECONEXIÓN ACTIVADA PARA PREVENCIÓN DE QUEDAS.."
+    node index.js
+    sleep 1
+
+done
